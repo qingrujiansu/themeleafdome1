81ef5bfce78ce08c300d08331002776cf45d30d2
qingrujiansu <2789930689@qq.com>
2022-08-28 19:18:25 +0800

    first commit

---
+++ src/main/java/com/example/themeleafdome/ThemeleafdomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.themeleafdome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeleafdomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeleafdomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/themeleafdome/ThemeleafdomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.themeleafdome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeleafdomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
